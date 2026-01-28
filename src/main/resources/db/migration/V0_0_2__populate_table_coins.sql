29f25a214af4dbf01a377c78fe1eb8c839727820
rodneybarreto <rwbarreto@gmail.com>
2026-01-28 11:10:52 -0300

    chore: renomeia code das moedas

--- src/main/resources/db/migration/V0_0_2__populate_table_coins.sql
+++ src/main/resources/db/migration/V0_0_2__populate_table_coins.sql
@@ -3,11 +3,11 @@ insert into coins
 values
     (default, '001', '1 centavo', 'Pedro Álvares Cabral, cobre sobre aço', 0.01),
     (default, '005', '5 centavos', 'Tiradentes, cobre sobre aço', 0.05),
-    (default, '005H', '5 centavos', 'produzida na Holanda', 0.05),
+    (default, '005h', '5 centavos', 'produzida na Holanda', 0.05),
     (default, '010', '10 centavos', 'D. Pedro I, bronze sobre aço', 0.10),
     (default, '025', '25 centavos', 'Deodoro da Fonseca, bronze sobre aço', 0.25),
-    (default, '050cp', '50 centavos', 'Barão do Rio Branco, cuproníquel', 0.50),
+    (default, '050cn', '50 centavos', 'Barão do Rio Branco, cuproníquel', 0.50),
     (default, '050ai', '50 centavos', 'Barão do Rio Branco, aço inoxidável', 0.50),
-    (default, '050H', '50 centavos', 'produzida na Holanda', 0.50),
-    (default, '1acp', '1 Real', 'bimetálica, anel de alpaca + núcleo de cuproníquel', 1.00),
+    (default, '050h', '50 centavos', 'produzida na Holanda', 0.50),
+    (default, '1acn', '1 Real', 'bimetálica, anel de alpaca + núcleo de cuproníquel', 1.00),
     (default, '1bai', '1 Real', 'bimetálica, anel de bronze + núcleo de aço inoxidável', 1.00);
