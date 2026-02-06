69a624937e2e26d766c2da76820fdb5f2e722c3b
rodneybarreto <rwbarreto@gmail.com>
2026-02-06 20:16:08 -0300

    chore: acrescenta mais moedas nos scripts sql

--- src/main/resources/db/migration/V2__populate_table_coins.sql
+++ src/main/resources/db/migration/V2__populate_table_coins.sql
@@ -1,13 +1,19 @@
 insert into coins
     (id, code, name, description, value)
 values
-    (default, '001', '1 centavo', 'Pedro Álvares Cabral, cobre sobre aço', 0.01),
-    (default, '005', '5 centavos', 'Tiradentes, cobre sobre aço', 0.05),
-    (default, '005h', '5 centavos', 'produzida na Holanda', 0.05),
-    (default, '010', '10 centavos', 'D. Pedro I, bronze sobre aço', 0.10),
-    (default, '025', '25 centavos', 'Deodoro da Fonseca, bronze sobre aço', 0.25),
+    (default, '001ca', '1 centavo', 'Pedro Álvares Cabral, cobre sobre aço', 0.01),
+    (default, '001ai', '1 centavo', 'Aço inox, Efígie da República', 0.01),
+    (default, '005ca', '5 centavos', 'Tiradentes, cobre sobre aço', 0.05),
+    (default, '005ai', '5 centavos', 'Aço inox, Efígie da República', 0.05),
+    (default, '005H', '5 centavos', 'Produzida na Holanda. Apresenta a letra "A" abaixo da data.', 0.05),
+    (default, '010ba', '10 centavos', 'D. Pedro I, bronze sobre aço', 0.10),
+    (default, '010ai', '10 centavos', 'Aço inox, Efígie da República', 0.10),
+    (default, '025ba', '25 centavos', 'Deodoro da Fonseca, bronze sobre aço', 0.25),
+    (default, '025ai', '25 centavos', 'Aço inox, Efígie da República', 0.25),
     (default, '050cn', '50 centavos', 'Barão do Rio Branco, cuproníquel', 0.50),
-    (default, '050ai', '50 centavos', 'Barão do Rio Branco, aço inoxidável', 0.50),
-    (default, '050h', '50 centavos', 'produzida na Holanda', 0.50),
+    (default, '050ai2', '50 centavos', 'Barão do Rio Branco, aço inoxidável', 0.50),
+    (default, '050ai', '50 centavos', 'Aço inox, Efígie da República', 0.50),
+    (default, '050H', '50 centavos', 'Produzida na Holanda. Apresenta a letra "A" abaixo da data.', 0.50),
     (default, '1acn', '1 Real', 'bimetálica, anel de alpaca + núcleo de cuproníquel', 1.00),
+    (default, '1ai', '1 Real', 'Aço inox, Efígie da República', 1.00),
     (default, '1bai', '1 Real', 'bimetálica, anel de bronze + núcleo de aço inoxidável', 1.00);
